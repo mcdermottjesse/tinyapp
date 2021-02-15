9c8b4530b2b6fd67a9f1f48f360b3aee4eb236d5
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-02-15 22:01:59 +0000

    added urls index

--- express_server.js
+++ express_server.js
@@ -9,13 +9,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
