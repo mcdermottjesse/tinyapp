d1f6b93889bf066dbe1c6418bd23ea6fbad6be6d
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-02-16 00:54:37 +0000

    added body-parser

--- express_server.js
+++ express_server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = 8080; // default port 8080
 
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.set("view engine", "ejs");
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
